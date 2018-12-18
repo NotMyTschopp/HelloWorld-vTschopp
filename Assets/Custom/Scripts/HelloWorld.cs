8f493574070f97c3ef5984925dcef9c247a213ed
Valerian Tschopp <valeriantschopp@Valerians-MacBook-Pro.local>
2018-12-18 10:04:57 +0100

    Added respond from world

--- Assets/Custom/Scripts/HelloWorld.cs
+++ Assets/Custom/Scripts/HelloWorld.cs
@@ -7,6 +7,7 @@ public class HelloWorld : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("Hello World!");
+        Debug.Log("Hello Human!");
 	}
 	
 	// Update is called once per frame
